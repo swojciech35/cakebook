5d60167ba5360909b13759bf100382a8f23ba747
swojciech35 <104626609+swojciech35@users.noreply.github.com>
2023-06-13 20:55:11 +0200

    add signup and login

--- cakebook/src/app/app-routing.module.ts
+++ cakebook/src/app/app-routing.module.ts
@@ -3,16 +3,31 @@ import {RouterModule, Routes} from '@angular/router';
 import {CakebookComponent} from "./components/cakebook/cakebook.component";
 import {CakebookHomeComponent} from "./components/cakebook-home/cakebook-home.component";
 import {CakebookItemDetailsComponent} from "./components/cakebook-item-details/cakebook-item-details.component";
+import {AuthGuard} from "./services/auth.guard";
+import {LoginComponent} from "./components/login/login.component";
+import {SignupComponent} from "./components/signup/signup.component";
 
 const routes: Routes = [{
   path: '',
-  component: CakebookHomeComponent
+  component: CakebookHomeComponent,
+
 
 },
   {
     path: 'recipe/detail/:id',
-    component: CakebookItemDetailsComponent
+    component: CakebookItemDetailsComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
   }
+
+
 
 
 ];
--- cakebook/src/app/app.module.ts
+++ cakebook/src/app/app.module.ts
@@ -1,25 +1,35 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CakebookComponent } from './components/cakebook/cakebook.component';
-import {HttpClientModule} from "@angular/common/http";
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {CakebookComponent} from './components/cakebook/cakebook.component';
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {DataService} from "./services/data.service";
-import { CakebookItemComponent } from './components/cakebook-item/cakebook-item.component';
-import { CakebookItemImageComponent } from './components/cakebook-item-image/cakebook-item-image.component';
-import { CakebookItemTextComponent } from './components/cakebook-item-text/cakebook-item-text.component';
-import { CakebookItemDirectionsComponent } from './components/cakebook-item-directions/cakebook-item-directions.component';
-import { CakebookItemIngredientsComponent } from './components/cakebook-item-ingredients/cakebook-item-ingredients.component';
-import { CakebookItemTitleComponent } from './components/cakebook-item-title/cakebook-item-title.component';
-import { CakebookItemRecipeinfoComponent } from './components/cakebook-item-recipeinfo/cakebook-item-recipeinfo.component';
-import { CakebookItemDetailsComponent } from './components/cakebook-item-details/cakebook-item-details.component';
-import { SummaryPipe } from './pipes/summary.pipe';
-import { SearchBarComponent } from './shared/search-bar/search-bar.component';
+import {CakebookItemComponent} from './components/cakebook-item/cakebook-item.component';
+import {CakebookItemImageComponent} from './components/cakebook-item-image/cakebook-item-image.component';
+import {CakebookItemTextComponent} from './components/cakebook-item-text/cakebook-item-text.component';
+import {
+  CakebookItemDirectionsComponent
+} from './components/cakebook-item-directions/cakebook-item-directions.component';
+import {
+  CakebookItemIngredientsComponent
+} from './components/cakebook-item-ingredients/cakebook-item-ingredients.component';
+import {CakebookItemTitleComponent} from './components/cakebook-item-title/cakebook-item-title.component';
+import {
+  CakebookItemRecipeinfoComponent
+} from './components/cakebook-item-recipeinfo/cakebook-item-recipeinfo.component';
+import {CakebookItemDetailsComponent} from './components/cakebook-item-details/cakebook-item-details.component';
+import {SummaryPipe} from './pipes/summary.pipe';
+import {SearchBarComponent} from './shared/search-bar/search-bar.component';
 import {FormsModule} from "@angular/forms";
-import { CakebookHomeComponent } from './components/cakebook-home/cakebook-home.component';
+import {CakebookHomeComponent} from './components/cakebook-home/cakebook-home.component';
 
-import { FilterTitlePipe } from './pipes/filter-title.pipe';
+import {FilterTitlePipe} from './pipes/filter-title.pipe';
+import {AuthService} from "./services/auth.service";
+import {AuthInterceptor} from "./services/auth/auth.interceptor";
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
   declarations: [
@@ -37,7 +47,9 @@ import { FilterTitlePipe } from './pipes/filter-title.pipe';
     SearchBarComponent,
     CakebookHomeComponent,
 
-    FilterTitlePipe
+    FilterTitlePipe,
+     LoginComponent,
+     SignupComponent
   ],
   imports: [
     BrowserModule,
@@ -45,7 +57,13 @@ import { FilterTitlePipe } from './pipes/filter-title.pipe';
     HttpClientModule,
     FormsModule
   ],
-  providers: [ DataService],
+  providers: [DataService, AuthService, {
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptor,
+    multi: true
+  }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
